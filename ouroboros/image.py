b2bedd9580595f461335ec886df8b3818696dc73
circa10a <caleblemoine@gmail.comS>
2018-09-18 18:35:49 -0500

    use sha check function

--- ouroboros/image.py
+++ ouroboros/image.py
@@ -6,6 +6,6 @@ def pull_latest(image):
     """Return tag of latest image pulled. Will be a version, not latest"""
     return client.images.pull(image.tags[0].split(':')[0] + ':latest')
 
-def is_image_up_to_date(old_sha, new_sha):
+def is_up_to_date(old_sha, new_sha):
     """Returns boolean if old and new image digests match"""
     return old_sha == new_sha
--- ouroboros/main.py
+++ ouroboros/main.py
@@ -11,7 +11,7 @@ def main():
         current_image = client.images.get(running_container["ImageID"])
         latest_image = image.pull_latest(current_image)
         # if current running container is running latest image
-        #print(current_image.id == latest_image.id)
+        print(image.is_up_to_date(current_image.id, latest_image.id))
         # new container object to create new container from
         print(containers.new_container_properties(running_container, latest_image.tags[0]).__dict__)
 if __name__ == "__main__":
