777a4283cf28418026dc7c8adc4e2b4578324222
circa10a <caleblemoine@gmail.comS>
2018-09-23 00:46:28 -0500

    add sweet logging

---
+++ ouroboros/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+def set_logger(level='info'):
+    """Set log level (Default=info"""
+    levels = {
+        'notset': logging.NOTSET,
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warn': logging.WARN,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
+    }
+    if level not in levels:
+        level = 'info'
+    return logging.basicConfig(format='[%(levelname)-4s] %(asctime)s %(message)s', level=(levels.get(level.lower())), datefmt='%Y-%m-%d %H:%M:%S')
--- ouroboros/main.py
+++ ouroboros/main.py
@@ -1,24 +1,27 @@
 import docker
 import containers, image
 import datetime
+from logger import set_logger
+import logging
+
 
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 def main():
     if not containers.running_properties():
-        print('[INFO] {0:%Y-%m-%d %H:%M:%S} No containers are running'.format(datetime.datetime.now()))
+        logging.info('No containers are running')
     else:
         for running_container in containers.running_properties():
             current_image = client.images.get(running_container["ImageID"])
             try:
                 latest_image = image.pull_latest(current_image)
             except docker.errors.APIError as e:
-                print(e)
+                logging.error(e)
                 break
             # if current running container is running latest image
             if not image.is_up_to_date(current_image.id, latest_image.id):
-                print('[INFO] {0:%Y-%m-%d %H:%M:%S} {1} is out of date'.format(datetime.datetime.now(), str(running_container["Names"][0]).replace('/','')))
+                logging.info(str('{} will be updated').format(running_container["Names"][0]).replace('/',''))
                 # new container object to create new container from
                 new_config = containers.NewContainerProperties(running_container, latest_image.tags[0])
                 containers.stop(running_container)
@@ -26,7 +29,8 @@ def main():
                 new_container = containers.create_new_container(new_config.__dict__)
                 containers.start(new_container)
                 image.remove(current_image.id)
-        print('[INFO] {0:%Y-%m-%d %H:%M:%S} All containers up to date'.format(datetime.datetime.now()))
+        logging.info('All containers up to date')
 
 if __name__ == "__main__":
-   main()
+    set_logger('debug')
+    main()
