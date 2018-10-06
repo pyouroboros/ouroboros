d6a5b487dafda5d4b1cd08757e9a72e734f6322d
circa10a <caleblemoine@gmail.comS>
2018-10-06 14:04:12 -0500

    improve run tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-pip install -r requirements-dev.txt
+module_list=$(pip list | awk '{print $1'})
+
+# Install modules that arent present, save time instead of reinstalling everytime
+for i in $(cat requirements-dev.txt);
+  do
+    if ! grep -x $i <<< "$module_list" 1>/dev/null; then
+        pip install $i
+    fi
+  done
+
+#pip install -r requirements-dev.txt
 export PYTHONPATH=$(pwd)/ouroboros
 $(which python3) -m pytest -v
