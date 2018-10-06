eed903665f8e9c822ea366e818bd578d76e96213
circa10a <caleblemoine@gmail.comS>
2018-10-06 08:38:22 -0500

    added support for private registries

--- ouroboros/image.py
+++ ouroboros/image.py
@@ -1,11 +1,18 @@
 import logging
 import cli
+from os import environ
+
+def check_credentials():
+    """Returns dict of credentials if environment variable 'REPO_USER' and 'REPO_PASS' are set"""
+    if environ.get('REPO_USER') and environ.get('REPO_PASS') is not None:
+        return { 'username': environ['REPO_USER'], 'password': environ['REPO_PASS'] }
+    return {}
 
 def pull_latest(image):
     """Return tag of latest image pulled"""
     latest_image = image['RepoTags'][0].split(':')[0] + ':latest'
     logging.debug(('Pulling image: {}').format(latest_image))
-    cli.api_client.pull(latest_image)
+    cli.api_client.pull(latest_image, auth_config=check_credentials())
     return cli.api_client.inspect_image(latest_image)
 
 def is_up_to_date(old_sha, new_sha):
