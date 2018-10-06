ee67fd5b6c0c6d1442a27e7ecd97b1e6c39112e4
circa10a <caleblemoine@gmail.comS>
2018-10-06 12:20:53 -0500

    fix typo

--- ouroboros/cli.py
+++ ouroboros/cli.py
@@ -26,7 +26,7 @@ def parser():
     parser = argparse.ArgumentParser(description='ouroboros', epilog='Example: python3 main.py -u tcp://1.2.3.4:5678 -i 20 -m container1 container2 -d warn')
     parser.add_argument('-u', '--url', help='Url for tcp host (defaults to "unix://var/run/docker.sock")', required=False)
     parser.add_argument('-i','--interval', type=int, help='Interval in seconds between checking for updates (defaults to 300s)', required=False)
-    parser.add_argument('-m','--monitor', nargs='+', help='Which container to monitor (defaults to all found).', required=False)
+    parser.add_argument('-m','--monitor', nargs='+', help='Which container to monitor (defaults to all running).', required=False)
     parser.add_argument('-d','--debug', choices=['notset','debug','info', 'warn', 'error', 'critical'], help='Change logger mode to debug (defaults to warn)', required=False)
     args = parser.parse_args()
     if args.url:
