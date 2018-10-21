72097f2ef81757cca9ec428ffd72ef9c88da61ce
circa10a <caleblemoine@gmail.comS>
2018-10-21 01:34:11 -0500

    tags

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 VERSION=$(grep -i version ouroboros/__init__.py | awk -F= '{gsub("\047",""); print $2}')
+# Docker
 USER='circa10a'
 PROJECT='ouroboros'
 NAMESPACE=${USER}/${PROJECT}
@@ -11,3 +12,9 @@ docker push $NAMESPACE:latest
 # Versioned
 docker tag $NAMESPACE:latest $NAMESPACE:$VERSION
 docker push $NAMESPACE:$VERSION
+
+# Git tags
+git remote set-url origin https://$USER:$github_api_key@github.com/$USER/$PROJECT.git
+git tag $VERSION
+git push --tags
+[ -f ~/.git/config ] && rm ~/.git/config
