505cb28438b93f8dc3913ab1b1217a4149605214
Traci Kamp <traci.kamp@gmail.com>
2018-10-21 18:10:29 -0500

    Remove required=False since that's the default

--- ouroboros/cli.py
+++ ouroboros/cli.py
@@ -37,12 +37,12 @@ def parser(sysargs):
     """Declare command line options"""
     global host, interval, monitor, loglevel, api_client, run_once, cleanup
     parser = argparse.ArgumentParser(description='ouroboros', epilog='Example: python3 main.py -u tcp://1.2.3.4:5678 -i 20 -m container1 container2 -l warn')
-    parser.add_argument('-u', '--url', help='Url for tcp host (defaults to "unix://var/run/docker.sock")', required=False)
-    parser.add_argument('-i', '--interval', type=int, help='Interval in seconds between checking for updates (defaults to 300s)', required=False)
-    parser.add_argument('-m', '--monitor', nargs='+', help='Which container to monitor (defaults to all running).', required=False)
-    parser.add_argument('-l', '--loglevel', choices=['notset', 'debug', 'info', 'warn', 'error', 'critical'], help='Change logger mode (defaults to info)', required=False)
-    parser.add_argument('-r', '--runonce', help='Only run ouroboros once then exit', action='store_true', required=False)
-    parser.add_argument('-c', '--cleanup', help='Remove old images after updating', action='store_true', required=False)
+    parser.add_argument('-u', '--url', help='Url for tcp host (defaults to "unix://var/run/docker.sock")')
+    parser.add_argument('-i', '--interval', type=int, help='Interval in seconds between checking for updates (defaults to 300s)')
+    parser.add_argument('-m', '--monitor', nargs='+', help='Which container to monitor (defaults to all running).')
+    parser.add_argument('-l', '--loglevel', choices=['notset', 'debug', 'info', 'warn', 'error', 'critical'], help='Change logger mode (defaults to info)')
+    parser.add_argument('-r', '--runonce', help='Only run ouroboros once then exit', action='store_true')
+    parser.add_argument('-c', '--cleanup', help='Remove old images after updating', action='store_true')
     args = parser.parse_args(sysargs)
 
     if args.url:
