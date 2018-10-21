81c80fa3d9824bd43f5d6a6883f9e5194cc1799b
Traci Kamp <traci.kamp@gmail.com>
2018-10-21 18:35:56 -0500

    Specify defaults in the argument definitions

--- ouroboros/cli.py
+++ ouroboros/cli.py
@@ -36,13 +36,19 @@ def get_interval_env():
 def parser(sysargs):
     """Declare command line options"""
     global host, interval, monitor, loglevel, api_client, run_once, cleanup
-    parser = argparse.ArgumentParser(description='ouroboros', epilog='Example: python3 main.py -u tcp://1.2.3.4:5678 -i 20 -m container1 container2 -l warn')
+    parser = argparse.ArgumentParser(description='ouroboros',
+                                     epilog='Example: python3 main.py -u tcp://1.2.3.4:5678 -i 20 -m container1 container2 -l warn')
     parser.add_argument('-u', '--url', help='Url for tcp host (defaults to "unix://var/run/docker.sock")')
-    parser.add_argument('-i', '--interval', type=int, help='Interval in seconds between checking for updates (defaults to 300s)')
-    parser.add_argument('-m', '--monitor', nargs='+', help='Which container to monitor (defaults to all running).')
-    parser.add_argument('-l', '--loglevel', choices=['notset', 'debug', 'info', 'warn', 'error', 'critical'], help='Change logger mode (defaults to info)')
-    parser.add_argument('-r', '--runonce', help='Only run ouroboros once then exit', action='store_true')
-    parser.add_argument('-c', '--cleanup', help='Remove old images after updating', action='store_true')
+    parser.add_argument('-i', '--interval', type=int, default=get_interval_env() or defaults.INTERVAL,
+                        help='Interval in seconds between checking for updates (defaults to 300s)')
+    parser.add_argument('-m', '--monitor', nargs='+', default=environ.get('MONITOR') or [],
+                        help='Which container to monitor (defaults to all running).')
+    parser.add_argument('-l', '--loglevel', choices=['notset', 'debug', 'info', 'warn', 'error', 'critical'],
+                        default=environ.get('LOGLEVEL') or 'info', help='Change logger mode (defaults to info)')
+    parser.add_argument('-r', '--runonce', default=environ.get('RUNONCE') or False,
+                        help='Only run ouroboros once then exit', action='store_true')
+    parser.add_argument('-c', '--cleanup', default=environ.get('CLEANUP') or False,
+                        help='Remove old images after updating', action='store_true')
     args = parser.parse_args(sysargs)
 
     if args.url:
@@ -50,10 +56,10 @@ def parser(sysargs):
         if not checkURI(host):
             host = defaults.LOCAL_UNIX_SOCKET
 
-    interval = args.interval or get_interval_env() or defaults.INTERVAL
-    monitor = args.monitor or environ.get('MONITOR') or []
-    loglevel = args.loglevel or environ.get('LOGLEVEL') or 'info'
-    run_once = args.runonce or environ.get('RUNONCE') or False
-    cleanup = args.cleanup or environ.get('CLEANUP') or False
+    interval = args.interval
+    monitor = args.monitor
+    loglevel = args.loglevel
+    run_once = args.runonce
+    cleanup = args.cleanup
     api_client = docker.APIClient(base_url=host)
     return args
