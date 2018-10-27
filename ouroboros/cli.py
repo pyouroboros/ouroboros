7a16954ff8b748b1ab6c44038416a7f08faf9053
Traci Kamp <traci.kamp@gmail.com>
2018-10-27 09:42:27 -0500

    Update the logic to handle empty string

--- ouroboros/cli.py
+++ ouroboros/cli.py
@@ -52,7 +52,9 @@ def parse(sysargs):
     global api_client
     parser = argparse.ArgumentParser(description='ouroboros',
                                      epilog='Example: python3 main.py -u tcp://1.2.3.4:5678 -i 20 -m container1 container2 -l warn')
-    parser.add_argument('-u', '--url', help='Url for tcp host (defaults to "unix://var/run/docker.sock")')
+
+    parser.add_argument('-u', '--url', default=defaults.LOCAL_UNIX_SOCKET,
+                        help='Url for tcp host (defaults to "unix://var/run/docker.sock")')
 
     parser.add_argument('-i', '--interval', type=int, default=get_interval_env() or defaults.INTERVAL, dest="interval",
                         help='Interval in seconds between checking for updates (defaults to 300s)')
@@ -71,9 +73,11 @@ def parse(sysargs):
                         help='Remove old images after updating', action='store_true')
     args = parser.parse_args(sysargs)
 
-    if args.url:
-        if not checkURI(args.url):
-            args.url = defaults.LOCAL_UNIX_SOCKET
+    if not args.url:
+        args.url = defaults.LOCAL_UNIX_SOCKET
+    else:
+        if args.url is not defaults.LOCAL_UNIX_SOCKET:
+            args.url = args.url if checkURI(args.url) else defaults.LOCAL_UNIX_SOCKET
 
     api_client = docker.APIClient(base_url=args.url)
     return args
