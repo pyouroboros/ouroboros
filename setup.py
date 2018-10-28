7bb8e02c023b8dbb3beec369c43b0787939b9e9d
circa10a <caleblemoine@gmail.comS>
2018-10-27 23:52:38 -0500

    add more detail to setup.py, dont fail build if package already exists in pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup, find_packages
 
+
+def readme():
+    with open('./README.md') as f:
+        return f.read()
+
+
+def read_reqs(requirements):
+    with open(requirements) as f:
+        return f.read().splitlines()
+
+
 setup(
     name='ouroboros-cli',
     version='0.2.1',
+    description='Automatically update running docker containers',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     url='https://github.com/circa10a/ouroboros',
+    license='MIT',
     classifiers=['Programming Language :: Python'],
     packages=find_packages(exclude=['tests']),
     scripts=['ouroboros/ouroboros'],
-    install_requires=['schedule', 'docker'],
-    tests_require=['pytest', 'pytest-mock', 'pytest-cov', 'codecov'],
+    install_requires=read_reqs(requirements='./requirements.txt'),
+    tests_require=read_reqs(requirements='./requirements-dev.txt'),
     python_requires='>=3.0'
 )
