312fe94e491da9aaeccfbfebe7b8a676ae636e61
Traci Kamp <traci.kamp@gmail.com>
2018-11-01 21:08:21 -0500

    Add test for reconciliation

--- tests/unit/container_test.py
+++ tests/unit/container_test.py
@@ -25,6 +25,21 @@ def test_get_name(fake_container):
     assert container.get_name(fake_container) == 'testName1'
 
 
+@pytest.mark.parametrize('monitor_args, ignore_args, mon_result, expected_caplog', [
+    (["test1", "test2"], ["test3", "test4"], ["test1", "test2"], False),
+    (["test1"], ["test1", "test2"], [], True),
+    ([], [], [], False),
+    ([], ["test1"], [], False),
+    (["test1"], [], ["test1"], False)
+])
+def test_reconcile_monitor_ignore(caplog, monitor_args, ignore_args, mon_result, expected_caplog):
+    result, _ = container.reconcile_monitor_ignore(monitor_args, ignore_args)
+    assert result == mon_result
+    if expected_caplog:
+        assert "specified in monitor and ignore." in caplog.text
+        assert "Container(s) will not be updated." in caplog.text
+
+
 def test_to_monitor(mocker):
     mock_client = mocker.Mock(spec=docker.APIClient)
     mock_client.base_url = ouroboros.defaults.LOCAL_UNIX_SOCKET
