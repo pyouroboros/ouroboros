5483d3c59ee8c2678b43cbc4c8b9c326808646f8
Traci Kamp <traci.kamp@gmail.com>
2018-11-03 13:41:33 -0500

    Test exceptions for 100% code coverage

--- tests/unit/main_unit_test.py
+++ tests/unit/main_unit_test.py
@@ -55,3 +55,29 @@ def test_main_full(fake_namespace, fake_api, caplog):
     fake_api.remove_container.assert_called_once()
     fake_api.create_container.assert_called_once()
     fake_api.start.assert_called_once()
+
+
+def test_main_exception(fake_namespace, fake_api, caplog):
+    caplog.set_level(logging.INFO)
+
+    fake_namespace.run_once = True
+    fake_namespace.monitor = ["testName1"]
+    fake_namespace.ignore = ["derp"]
+    fake_namespace.cleanup = True
+
+    fake_api.inspect_container.return_value = container_object  # called twice
+    fake_api.containers.return_value = [container_object]  # called twice
+    fake_api.inspect_image.return_value = {'RepoTags': ["repo:1.1"], 'Id': '1'} # called once
+
+    fake_api.pull.side_effect = docker.errors.APIError("I blew up!!")
+    fake_api.create_container.return_value = {'Id': '2'}
+
+    with pytest.raises(SystemExit) as e:
+        main(args=fake_namespace, api_client=fake_api)
+
+    assert e.type == SystemExit
+    assert e.value.code == 0
+    assert 2 == fake_api.containers.call_count
+    assert 2 == fake_api.inspect_container.call_count
+    fake_api.inspect_image.assert_called_once()
+    fake_api.pull.assert_called_once()
