0e7406cc73db71e246c3bba1bedead2a8d90688b
Christopher Thomas <chris0392@gmail.com>
2019-01-02 09:26:47 -0600

    added method to get pod container sha

--- ouroboros/deployment.py
+++ ouroboros/deployment.py
@@ -31,7 +31,32 @@ def updateDeployment(imageTag, deploymentName, namespace):
                     container.image = base_image + ":latest"
                     new_containers.append(container)
                 deployment.spec.template.spec.containers = new_containers
+                api_instance.patch_namespaced_deployment(deployment.metdata.name, deployment.metadata.namespace, deployment)
                     
+def updateAllDeployments():
+    api_instance = client.ExtensionsV1beta1Api()
+    namespaces = getAllNamespaces()
+    for namespace in namespaces:
+        if namespace.metadata.name == "kube-system":
+            break
+        deployments = api_instance.list_namespaced_deployment(namespace=namespace)
+        for deployment in deployments:
+                old_containers = deployment.spec.template.spec.containers
+                new_containers= []
+                for container in old_containers:
+                    base_image = container.image.split(":")[0]
+                    container.image = base_image + ":latest"
+                    new_containers.append(container)
+                deployment.spec.template.spec.containers = new_containers
+                api_instance.patch_namespaced_deployment(deployment.metdata.name, deployment.metadata.namespace, deployment)
+        
+def getPodContainerSha(pod, deploymentName):
+    api_instance = client.CoreV1Api()
+    pods = api_instance.list_pod_for_all_namespaces()
+    for pod in pods.items:
+        for status in pod.status.container_statuses:
+            if(pod.metadata.name == pod):
+                return status.image_id.split("@")[1]
 
 def getAllNamespaces():
     api_instance = client.CoreV1Api()
