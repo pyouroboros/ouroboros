62f447ace5e830934d8fed68629d34469e9f1f40
circa10a <caleblemoine@gmail.com>
2019-02-10 11:43:58 -0600

    search is 4x than comparing .upper(), who knew

--- pyouroboros/logger.py
+++ pyouroboros/logger.py
@@ -49,5 +49,5 @@ class OuroborosLogger(object):
         self.logger.addHandler(console_logger)
 
         # Less verbose apscheduler logging
-        if level.upper() == 'INFO':
+        if level in ['info', 'INFO']:
             getLogger('apscheduler').setLevel(WARNING)
