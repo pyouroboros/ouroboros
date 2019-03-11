a50fcb1aaf560d3f1b968074ca80e2b1136f377d
Caleb Lemoine <21261388+circa10a@users.noreply.github.com>
2019-03-10 19:49:36 -0500

    Arg skip startup notifications (#261)
    * add option to skip startup notifications
    
    * fix flake8
    
    * fix flake8

--- pyouroboros/config.py
+++ pyouroboros/config.py
@@ -8,7 +8,7 @@ class Config(object):
                'PROMETHEUS_PORT', 'NOTIFIERS', 'REPO_USER', 'REPO_PASS', 'CLEANUP', 'RUN_ONCE', 'CRON',
                'INFLUX_URL', 'INFLUX_PORT', 'INFLUX_USERNAME', 'INFLUX_PASSWORD', 'INFLUX_DATABASE', 'INFLUX_SSL',
                'INFLUX_VERIFY_SSL', 'DATA_EXPORT', 'SELF_UPDATE', 'LABEL_ENABLE', 'DOCKER_TLS', 'LABELS_ONLY',
-               'DRY_RUN', 'HOSTNAME', 'DOCKER_TLS_VERIFY', 'SWARM']
+               'DRY_RUN', 'HOSTNAME', 'DOCKER_TLS_VERIFY', 'SWARM', 'SKIP_STARTUP_NOTIFICATIONS']
 
     hostname = environ.get('HOSTNAME')
     interval = 300
@@ -45,6 +45,7 @@ class Config(object):
     influx_database = None
 
     notifiers = []
+    skip_startup_notifications = False
 
     def __init__(self, environment_vars, cli_args):
         self.cli_args = cli_args
@@ -91,7 +92,8 @@ class Config(object):
                     except ValueError as e:
                         print(e)
                 elif option in ['CLEANUP', 'RUN_ONCE', 'INFLUX_SSL', 'INFLUX_VERIFY_SSL', 'DRY_RUN', 'SWARM',
-                                'SELF_UPDATE', 'LABEL_ENABLE', 'DOCKER_TLS', 'LABELS_ONLY', 'DOCKER_TLS_VERIFY']:
+                                'SELF_UPDATE', 'LABEL_ENABLE', 'DOCKER_TLS', 'LABELS_ONLY', 'DOCKER_TLS_VERIFY',
+                                'SKIP_STARTUP_NOTIFICATIONS']:
                     if env_opt.lower() in ['true', 'yes']:
                         setattr(self, option.lower(), True)
                     elif env_opt.lower() in ['false', 'no']:
